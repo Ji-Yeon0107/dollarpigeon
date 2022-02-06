4c697ac4f933b75888bd35f78c14bbd0c8f92e36
BakJiYeon <jiyeon@bagjiyeon-ui-MacBookPro.local>
2022-02-06 21:40:48 +0900

    1.2.0 pause available, html structure updated

--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
-const section = document.querySelector("section");
-const startButton = document.querySelector(".start__button");
-const restartButton = document.querySelector(".restart__button");
-const time = document.querySelector(".time");
-const count = document.querySelector(".count");
-const sound = document.querySelector(".sound");
-const muted = document.querySelector(".muted");
-const targetContainer = document.querySelector(".target__container");
-const clearMessage = document.querySelector(".clear__message");
-const tutorial = document.querySelector(".tutorial");
+const section = document.querySelector(".game");
+const startButton = document.querySelector(".game__start-button");
+const pauseButton = document.querySelector(".game__pause");
+const restartButton = document.querySelector(".game__restart-button");
+const resumeButton = document.querySelector(".game__resume-button");
+const time = document.querySelector(".game__timer");
+const count = document.querySelector(".game__count");
+const sound = document.querySelector(".game__sound");
+const muted = document.querySelector(".game__muted");
+const targetContainer = document.querySelector(".game__target-container");
+const clearMessage = document.querySelector(".game__message");
+const tutorial = document.querySelector(".game__tutorial");
 const bgSound = new Audio("../sound/bg.mp3");
 const clearSound = new Audio("../sound/game_win.mp3");
 const mosquitoSound = new Audio("../sound/carrot_pull.mp3");
 const watermelonSound = new Audio("../sound/bug_pull.mp3");
 const alertSound = new Audio("../sound/alert.wav");
+let TARGET_COUNT = 0;
+let LEFTTIME;
+let TIMER;
+
+count.innerText = TARGET_COUNT;
 
 sound.addEventListener("click", () => {
   bgSound.pause();
@@ -25,29 +32,20 @@ muted.addEventListener("click", () => {
   sound.style.display = "block";
 });
 
-let countNumber = 0;
-count.innerText = countNumber;
-let countTime = 0;
-time.innerText = `00:0${countTime}`;
-
-// 랜덤좌표에 들어갈 랜덤숫자 O
-const randomY = getRandomInt(45, 80);
-const randomX = getRandomInt(10, 80);
-
-function getRandomInt(min, max) {
+function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//랜덤하게 그림생성
 function putTargets() {
-  for (let i = 0; i < countNumber; i++) {
-    const randomY = getRandomInt(45, 80);
-    const randomX = getRandomInt(10, 80);
+  for (let i = 0; i < TARGET_COUNT; i++) {
+    // 랜덤좌표에 들어갈 랜덤숫자 O
+    const randomY = getRandomNumber(257, 400);
+    const randomX = getRandomNumber(90, 713);
 
-    const mosRandomY = getRandomInt(45, 80);
-    const mosRandomX = getRandomInt(10, 80);
+    const mosRandomY = getRandomNumber(257, 400);
+    const mosRandomX = getRandomNumber(90, 713);
 
     const newMosquito = document.createElement("img");
     newMosquito.setAttribute("class", "mosquito target");
@@ -55,8 +53,8 @@ function putTargets() {
     newMosquito.setAttribute("src", "./img/mosquito.png");
     newMosquito.setAttribute("alt", "mosquito");
     targetContainer.appendChild(newMosquito);
-    newMosquito.style.top = `${mosRandomY}%`;
-    newMosquito.style.left = `${mosRandomX}%`;
+    newMosquito.style.top = `${mosRandomY}px`;
+    newMosquito.style.left = `${mosRandomX}px`;
 
     const newWatermelon = document.createElement("img");
     newWatermelon.setAttribute("class", "watermelon target");
@@ -64,38 +62,36 @@ function putTargets() {
     newWatermelon.setAttribute("src", "./img/watermelon.png");
     newWatermelon.setAttribute("alt", "watermelon");
     targetContainer.appendChild(newWatermelon);
-    newWatermelon.style.top = `${randomY}%`;
-    newWatermelon.style.left = `${randomX}%`;
+    newWatermelon.style.top = `${randomY}px`;
+    newWatermelon.style.left = `${randomX}px`;
   }
 }
-
 startButton.addEventListener("click", (event) => {
   bgSound.play();
   tutorial.innerText = "Hit mosquitoes!";
-  tutorial.className = "tutorial move";
+  tutorial.className = "game__tutorial move";
   startButton.style.display = "none";
-  countNumber = 8;
-  count.innerText = countNumber;
+  pauseButton.style.display = "block";
+  TARGET_COUNT = 8;
+  count.innerText = TARGET_COUNT;
   targetContainer.style.opacity = "100%";
   putTargets();
   // 1초마다 1씩 줄어들게
-  countTime = "7";
-  handleCountTimeout();
+  LEFTTIME = "7";
+  handleTimer();
 });
 
-let countTimeout;
-
-function handleCountTimeout() {
-  const originalTargetNumber = countNumber;
-  countTimeout = setInterval(() => {
-    countTime--;
-    time.innerText = `00:0${countTime}`;
-    if (countTime === 3) {
+function handleTimer() {
+  const originalTargetNumber = TARGET_COUNT;
+  TIMER = setInterval(() => {
+    LEFTTIME--;
+    time.innerText = `00:0${LEFTTIME}`;
+    if (LEFTTIME === 3) {
       alertSound.play();
       time.style.color = "red";
     }
-    if (countTime === 0) {
-      clearInterval(countTimeout);
+    if (LEFTTIME === 0) {
+      clearInterval(TIMER);
       restartButton.style.display = "block";
       targetContainer.style.opacity = "0";
       clearMessage.innerHTML = "Try again";
@@ -105,24 +101,26 @@ function handleCountTimeout() {
       for (let i = 0; i < target.length; i++) {
         targetContainer.removeChild(target[i]);
       }
-      countNumber = originalTargetNumber;
+      TARGET_COUNT = originalTargetNumber;
+      pauseButton.style.display = "none";
     }
   }, 1000);
 }
 
 restartButton.addEventListener("click", (event) => {
   targetContainer.style.pointerEvents = "auto";
+  pauseButton.style.display = "block";
   time.style.color = "inherit";
   restartButton.style.display = "none";
-  count.innerText = countNumber;
-  countTime = "7";
-  time.innerText = `00:0${countTime}`;
+  count.innerText = TARGET_COUNT;
+  LEFTTIME = "7";
+  time.innerText = `00:0${LEFTTIME}`;
   targetContainer.style.opacity = "100%";
   clearMessage.innerHTML = "";
 
   //그림재배치
   putTargets();
-  handleCountTimeout();
+  handleTimer();
 });
 
 onClickTarget(targetContainer);
@@ -152,10 +150,10 @@ function onClickTarget(targetEle) {
     if (event.target.dataset.name === "mosquito") {
       mosquitoSound.play();
       event.target.style.display = "none";
-      countNumber--;
-      if (countNumber === 0) {
+      TARGET_COUNT--;
+      if (TARGET_COUNT === 0) {
         //시간카운트 멈추기
-        clearInterval(countTimeout);
+        clearInterval(TIMER);
         clearSound.play();
 
         const restartTerm = setTimeout(() => {
@@ -165,7 +163,7 @@ function onClickTarget(targetEle) {
 
           const target = document.querySelectorAll(".target");
           //재시작하면 갯수를 점차 늘리기
-          countNumber = refreshedTargetNumber + 2;
+          TARGET_COUNT = refreshedTargetNumber + 2;
 
           for (let i = 0; i < target.length; i++) {
             targetContainer.removeChild(target[i]);
@@ -177,7 +175,7 @@ function onClickTarget(targetEle) {
       watermelonSound.play();
       event.target.setAttribute("src", "./img/cracked.png");
       //시간카운트 멈추기
-      clearInterval(countTimeout);
+      clearInterval(TIMER);
       //클릭 막기
       targetContainer.style.pointerEvents = "none";
 
@@ -187,15 +185,26 @@ function onClickTarget(targetEle) {
 
         const target = document.querySelectorAll(".target");
 
-        countNumber = refreshedTargetNumber;
+        TARGET_COUNT = refreshedTargetNumber;
 
         for (let i = 0; i < target.length; i++) {
           targetContainer.removeChild(target[i]);
         }
       }, 400);
 
-      // restartTerm;
+      pauseButton.style.display = "none";
     }
-    count.innerText = countNumber;
+    count.innerText = TARGET_COUNT;
   });
 }
+
+pauseButton.addEventListener("click", () => {
+  clearInterval(TIMER);
+  resumeButton.style.display = "block";
+  targetContainer.style.pointerEvents = "none";
+});
+resumeButton.addEventListener("click", () => {
+  targetContainer.style.pointerEvents = "auto";
+  handleTimer();
+  resumeButton.style.display = "none";
+});
